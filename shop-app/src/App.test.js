f45f602439e852cd57ac8192f1292fbcd476d745
NLScotty <Scott.B.Normore@gmail.com>
2023-12-18 12:39:19 -0330

    Improved the Unit tests
    Improved Product List and Details unit tests  to be more re-usable and meaningful.

--- shop-app/src/App.test.js
+++ shop-app/src/App.test.js
@@ -4,10 +4,11 @@ import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import ProductDetails from './components/pages/ProductDetails';
 import ProductList from './components/pages/ProductList';
 
-// Might plan on redoing the test to fetch the first product/all products. then check to see if thier attributes match on what is on the page
-
 // test that checks if we can get an item from the database for the product details page
+
 test('Display ', async() => {
+    const response = await fetch(`http://localhost:5000/products/1`)
+    const pro1 = await response.json()
     render(
         <MemoryRouter initialEntries={['/product/1']}>
             <Routes>
@@ -15,13 +16,16 @@ test('Display ', async() => {
             </Routes>
         </MemoryRouter>
     );
-    await waitFor(()=>expect(screen.getByText("Product 1")))
-    expect(screen.getByText("Product 1")).toBeInTheDocument()
+    // wait for screen to render (I'm interested in a better way to do this if you know of one)
+    await waitFor(()=>expect(screen.getByText(pro1.name)))
+    expect(screen.getByText(pro1.name)).toBeInTheDocument()
 });
 
-// test that checks if we rendered the product page
+// test that checks if we rendered the product page with the items from the database.
 
 test('Products Page', async() => {
+  const response = await fetch(`http://localhost:5000/products`)
+  const data = await response.json()
   render(
     <MemoryRouter initialEntries={['/list']}>
       <Routes>
@@ -29,6 +33,10 @@ test('Products Page', async() => {
       </Routes>
     </MemoryRouter>
   );
-  await waitFor(()=>expect(screen.getByText("Product 1")))
-  expect(screen.getByText("Product 1")).toBeInTheDocument()
+  // wait for screen to render
+  await waitFor(()=>expect(screen.getByText(data[0].name)))
+  //we then check for each product
+  data.forEach(product => {
+    expect(screen.getByText(product.name)).toBeInTheDocument()
+  });
 });
